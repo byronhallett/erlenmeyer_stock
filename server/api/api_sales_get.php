05874eebc3b434fb8c52dd3a4ce637a23b5a3838
Byron Hallett <byron.james.h@gmail.com>
2015-11-08 01:40:56 +1100

    remove a typo from copy and paste

--- server/api/api_sales_get.php
+++ server/api/api_sales_get.php
@@ -7,32 +7,31 @@ include '../credentials.cfg.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: ".$conn->connect_error);
 }
 
 $data = json_decode(file_get_contents('php://input'), true);
 
 //Arrays for return data
-$salesData = array (
+$salesData = array(
 );
 
 // Will contain for each entry
-// 
+//
 // array ('itemId' => id,
-// 'itemName' = name,
-// 'salePrice' = price,
-// 'saleCost' = cost)
-
+// 'itemName' => name,
+// 'salePrice' => price,
+// 'saleCost' => cost)
 
 // query variables
 $dateFrom = $data['date_start'];
-$dateTo = $data['date_end'];
-$userId = $data['user_id'];
+$dateTo   = $data['date_end'];
+$userId   = $data['user_id'];
 
 // prepare query
 $sql = "SELECT s.sale_id, i.item_name, i.price, i.cost, c.category_name, s.sale_count, s.sale_date
-        FROM items i
-          inner join sale s on i.item_id = s.item_id
+        FROM sales s
+          inner join items i on s.item_id = i.item_id
           inner join categories c on i.category_id = c.category_id
         WHERE s.sale_date >= '$dateFrom'
         AND s.sale_date <= '$dateTo'
@@ -40,31 +39,31 @@ $sql = "SELECT s.sale_id, i.item_name, i.price, i.cost, c.category_name, s.sale_
         ORDER BY s.sale_date DESC";
 
 // Fetch data
-$sales = mysqli_query($conn,$sql);
+$salesData = mysqli_query($conn, $sql);
 
 // loop over the rows, outputting them
-if ($categories -> num_rows > 0) {
-  while ($row = mysqli_fetch_assoc($sales)) {
-    
-    $saleArray = array (
-      'itemId' => 0,
-      'itemName' => "",
-      'salePrice' => 0,
-      'saleCost' => 0
-    );
+if ($salesData->num_rows > 0) {
+	while ($row = mysqli_fetch_assoc($salesData)) {
 
-    $saleArray['itemId'] = $row['item_id'];
-    $saleArray['itemName'] = $row['item_name'];
-    $saleArray['salePrice'] = $row['sale_count'] * $row['price'];
-    $saleArray['saleCost'] = $row['sale_count'] * $row['cost'];
+		$thisSale = array(
+			'itemId'    => 0,
+			'itemName'  => "",
+			'salePrice' => 0,
+			'saleCost'  => 0,
+		);
 
-    array_push($salesData, $saleArray);
-  }
+		$saleArray['itemId']    = $row['item_id'];
+		$saleArray['itemName']  = $row['item_name'];
+		$saleArray['salePrice'] = $row['sale_count']*$row['price'];
+		$saleArray['saleCost']  = $row['sale_count']*$row['cost'];
 
-  $jsonReturn = json_encode($salesData);
-  echo $jsonReturn;
+		array_push($salesData, $thisSale);
+	}
+
+	$jsonReturn = json_encode($salesData);
+	echo $jsonReturn;
 
 } else {
-  echo "No Sales";
+	echo "No Sales";
 }
-?> 
+?>
