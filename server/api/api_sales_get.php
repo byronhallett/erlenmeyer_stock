e0fe4a30e37a2d81a60f11877f819f4ef9a1cc59
Byron Hallett <byron.james.h@gmail.com>
2015-11-08 02:02:55 +1100

    changes to array logic api php

--- server/api/api_sales_get.php
+++ server/api/api_sales_get.php
@@ -13,8 +13,7 @@ if ($conn->connect_error) {
 $data = json_decode(file_get_contents('php://input'), true);
 
 //Arrays for return data
-$salesData = array(
-);
+$salesData = array();
 
 // Will contain for each entry
 //
@@ -30,7 +29,7 @@ $userId   = $data['user_id'];
 
 // prepare query
 $sql = "SELECT s.sale_id, i.item_name, i.price, i.cost, c.category_name, s.sale_count, s.sale_date
-        FROM sales s
+        FROM sale s
           inner join items i on s.item_id = i.item_id
           inner join categories c on i.category_id = c.category_id
         WHERE s.sale_date >= '$dateFrom'
@@ -39,11 +38,11 @@ $sql = "SELECT s.sale_id, i.item_name, i.price, i.cost, c.category_name, s.sale_
         ORDER BY s.sale_date DESC";
 
 // Fetch data
-$salesData = mysqli_query($conn, $sql);
+$queryData = mysqli_query($conn, $sql);
 
 // loop over the rows, outputting them
-if ($salesData->num_rows > 0) {
-	while ($row = mysqli_fetch_assoc($salesData)) {
+if ($queryData->num_rows > 0) {
+	while ($row = mysqli_fetch_assoc($queryData)) {
 
 		$thisSale = array(
 			'itemId'    => 0,
@@ -52,10 +51,10 @@ if ($salesData->num_rows > 0) {
 			'saleCost'  => 0,
 		);
 
-		$saleArray['itemId']    = $row['item_id'];
-		$saleArray['itemName']  = $row['item_name'];
-		$saleArray['salePrice'] = $row['sale_count']*$row['price'];
-		$saleArray['saleCost']  = $row['sale_count']*$row['cost'];
+		$thisSale['itemId']    = $row['item_id'];
+		$thisSale['itemName']  = $row['item_name'];
+		$thisSale['salePrice'] = $row['sale_count']*$row['price'];
+		$thisSale['saleCost']  = $row['sale_count']*$row['cost'];
 
 		array_push($salesData, $thisSale);
 	}
